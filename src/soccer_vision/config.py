bab88dc0d6fbcc5703c437c2ac67f7591f8c9b0f
Grant Hohol <ghohol@wisc.edu>
2025-08-09 15:58:43 -0500

    scaffolding

---
+++ src/soccer_vision/config.py
@@ -0,0 +1,6 @@
+# src/soccer_cv/config.py
+
+from sports.config.soccer import SoccerPitchConfiguration
+
+# expose a single default config the library uses internally
+DEFAULT_CONFIG = SoccerPitchConfiguration()
